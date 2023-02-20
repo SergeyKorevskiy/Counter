4c2de8ea750930398fa09e3a6dd4101d9cf8e3d9
Sergei <korevskiy_s@mail.ru>
2023-02-21 01:00:48 +0300

    Размещен Label и кнопка счетчика, прописана логика. Добавлена кнопка сброса на 0.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var counterDisplay: UILabel!
+  
+  @IBOutlet weak var increaseButton: UIButton!
+  
+  var counterNumber: Int = 0
+  
+  func counterShow () { counterDisplay.text = "Значение счетчика: " + String(counterNumber)
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
+    counterShow()
   }
 
-
+  @IBAction func buttonDidTap(_ sender: Any) {
+    counterNumber += 1
+    print (counterNumber)
+    counterShow()
+  }
+  
+  
+  @IBAction func ResetButton(_ sender: Any) {
+    counterNumber = 0
+    counterShow()
+  }
+  
 }
 
